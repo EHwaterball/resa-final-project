a72f55e08c60cb34a49f9163299b65d7de568ff1
Evan Hwang <ehwang222@gmail.com>
2025-08-01 22:30:58 -0400

    finished lesson content. moving on to practice next

--- script.js
+++ script.js
@@ -138,7 +138,7 @@
         },
         {
             header: "\"But how do I use it??\"",
-            description: "Drop the 다, and add 아요 or 어요 to the end of the verb depending on the final vowel."
+            description: "Apply it to verbs- drop the 다, and add 아요 or 어요 to the end of the verb depending on the final vowel."
         },
         {
             header: "아요",
@@ -169,14 +169,116 @@
         },
         {
             header: "<span style='color: red; font-size: 5.0em'>NOPE.</span>",
-            description: "In Korean, we have to make sure the subject (나) and the object (사과) come before the verb (먹어요). Sentences in Korean always end with the verb!"
+            description: "In Korean, we have to make sure the subject (나) and the object (사과) come before the verb (먹어요). Sentences in Korean always end with the verb.<br><br>(This is largely one of the reasons why Korean is so hard to learn for English native speakers.)"
         },
         {
-            header: "So a more correct way to say \"I eat apple\" in Korean is 나 사과를 먹어요.",
-            description: ""
+            header: "So a more correct way to say \"I eat apple\" in Korean is 나 사과 먹어요.",
+            description: "Fun fact! You can switch around the subject and object in Korean sentences."
         }
     ];
 
+    const lessonContent8 = [
+        {
+            header: "",
+            description: "Wait... what?"
+        },
+        {
+            header: "If we can switch the subject and object, then how do we know what's actually happening?",
+            description: ""
+        },
+        {
+            header: "나 사과 먹어요 means I'm eating the apple, right?",
+            description: ""
+        },
+        {
+            header: "Or does it mean the apple is eating me!?",
+            description: ""
+        },
+        {
+            header: "",
+            description: "Luckily, Korean presents a solution that removes this ambiguity."
+        }
+    ];
+
+    const lessonContent9 = [
+        {
+            header: "Introducing the particles!",
+            description: "Korean has a couple of different suffixes that can be added to the end of a word to demarcate the roles words play in a sentence."
+        },
+        {
+            header: "As we wrapped up the third week at Ewha, we were given these particles to use in our sentences.",
+            description: "The ones that will pop up here are common and useful, but there are many more!"
+        },
+        {
+            header: "The first one is 은/는.",
+            description: "This is used to mark the topic of a sentence.<br><br>Ask yourself: What is this sentence about? What is doing the verb?<br><br>We use 은 if there is a final consonant, and 는 if there is no final consonant. <br>(and the best part about having that prerequisite page at the beginning is that we can assume you know what that is!)"
+        },
+        {
+            header: "So, 나 사과 먹어요 becomes 나<span style='color: teal'>는</span> 사과 먹어요.",
+            description: "This clarifies that 나 (I) is the one doing the 먹어요 (eating), and that we are safe from any particularly voracious apples."
+        }
+    ];
+
+    const lessonContent10 = [
+        {
+            header: "But it's still probably useful to clearly mark what the target of the verb is.",
+            description: "Ask yourself: What is being eaten?"
+        },
+        {
+            header: "Well obviously it's the apple.",
+            description: "So let's mark it."
+        },
+        {
+            header: "Add 을/를 to the end of a noun to mark it as the <span style='color: teal'>object</span> of the sentence.",
+            description: "Again, we use 을 if there is a final consonant, and 를 if there is no final consonant."
+        },
+        {
+            header: "So, 나는 사과 먹어요 becomes 나는 사과<span style='color: teal'>를</span> 먹어요.",
+            description: "This clarifies that 사과 (apple) is the target of the verb 먹어요 (it's being eaten)."
+        } 
+    ];
+
+    const lessonContent11 = [
+        {
+            header: "Now let's try to reconstruct the sentence \"I eat the apple\" from scratch.",
+            description: ""
+        },
+        {
+            header: "First: I'M doing the eating.",
+            description: "The word for 'I' is 나, and I'm the one who will do the eating, so I'll put 나는 as the first word."
+        },
+        {
+            header: "Secondly, what am I eating?",
+            description: "An apple, so I guess we could say it's the target of the verb \"eating.\" Thus, it's probably best to put in 사과 and attach the object particle 를 to it. <br><br>So far, we have 나는 사과를."
+        },
+        {
+            header: "Finally, what was the verb?",
+            description: "It was \"eating!\" So I'll conjugate 먹다 to 먹어요 and add it to the end of the sentence."
+        },
+        {
+            header: "So our final sentence is <span style='color: teal'>나는 사과를 먹어요</span>.",
+            description: "And this time, it's actually correct! Note that you'll sometimes notice the particles being omitted if it's especially obvious what role each word plays in the sentence."
+        },
+        {
+            header: "Now, let's move on to practice!",
+            description: "I fear I'm losing points for being too verbose and not relating it to my own experience enough, so let's go through some topical exercises."
+        }
+    ];
+
+    // const lessonContent12 = [
+    //     {
+    //         header: "Now, you're well equipped to construct basic Korean sentences with a subject, verb, and object.",
+    //         description: "할 수 있어요! (You can do it!)"
+    //     },
+    //     {
+    //         header: "But this is far from the end of your journey.",
+    //         description: "Again, this website doesn't attempt to present the entirety of Korean grammar, or even the entirety of the beginner Ewha Korean course! Topics that weren't covered in this lesson (but were in the Ewha curriculum) include:<br><br>1. Numbers (pg. 75-77 of Ewha's New Level 1 textbook)<br>2. Location particles (pg. 95-97 of Ewha's New Level 1 textbook)<br>3. Lots of other important vocabulary"
+    //     },
+    //     {
+    //         header: "Here's some recommendations for further study ",
+    //         description: "Thank you for taking the time to go through this website, and I hope you've found it useful!"
+    //     }
+    // ];
     const allLessonContent = [
         lessonContent,
         lessonContent2, 
@@ -184,7 +286,11 @@
         lessonContent4,
         lessonContent5,
         lessonContent6,
-        lessonContent7
+        lessonContent7,
+        lessonContent8,
+        lessonContent9,
+        lessonContent10,
+        lessonContent11
     ];
 
     function showHint() {
@@ -228,7 +334,7 @@
             lessonCounter = 0;
             currentLessonSet++;
             hideHint();
-            setTimeout(showNextElement, 500);
+            setTimeout(showNextElement, 0);
         });
         
         container.appendChild(nextButton);
@@ -284,14 +390,14 @@
             lessonCounter++;
             
             if (lessonCounter === currentContent.length && currentLessonSet < allLessonContent.length) {
-                setTimeout(showNextPageButton, 1000);
+                setTimeout(showNextPageButton, 0);
             }
         } else {
             return;
         }
     }
 
-    setTimeout(showNextElement, 500);
+    setTimeout(showNextElement, 0);
 
     document.addEventListener('keydown', (event) => {
         if (event.key === 'Enter') {
